c2a13c570df1a1acccba740751edbd5176c5869c
Bryan <bryanzin7@gmail.com>
2025-05-20 10:29:10 -0700

    nailed it

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+// const leapYears = function(num) {
+//     if (num % 4 === 0 || num % 400 === 0){
+//         if (num % 100 === 0){
+//             if (num % 400 === 0){
+//                 return true
+//             }else{
+//                 return false
+//             }
+//         }
+//         return true    
+//     }else {
+//         return false
+//     }
+// };
 
-};
-
+const leapYears = (year) => (year % 400 === 0 || (year % 100 === 0 && year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0));
+    
 // Do not edit below this line
 module.exports = leapYears;

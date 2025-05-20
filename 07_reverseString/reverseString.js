da643767324f6d0db13e88890a9877c7bdf163d7
Bryan <bryanzin7@gmail.com>
2025-05-19 21:29:29 -0700

    completed exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let arr = word.split('');
+    let rev = [];
+    for (let i = word.length-1; i >= 0; i--){
+        rev.push(arr[i]);
+    }
 
+    return rev.join('')
 };
 
 // Do not edit below this line

28930941508c69a4565aaf3eefb84098a1ef1a9b
Bryan <bryanzin7@gmail.com>
2025-05-20 09:47:32 -0700

    completed exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let arr = [num1, num2].sort((a,b) => a-b);
+    let sum = 0;
 
+    if (num1 < 0 || num2 < 0 || 
+        !Number.isInteger(num1) || !Number.isInteger(num2) || 
+        typeof num1 !== "number" || typeof num2 !== "number"){
+
+        return "ERROR"
+    }else{
+        for (let num = arr[0]; num<arr[1]+1;num++){
+            sum += num;
+        };
+        return sum
+    }
 };
 
 // Do not edit below this line

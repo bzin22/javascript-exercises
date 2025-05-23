30d8f6b62d59c7c7bf9cf2892db71f3262f0ee41
Bryan <bryanzin7@gmail.com>
2025-05-22 18:21:20 -0700

    completed assignment 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((age, person) => {
+        if (age.yearOfDeath == undefined){
+            age.yearOfDeath = new Date().getFullYear();
+        }
+        if (person.yearOfDeath == undefined){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        if (person.yearOfDeath - person.yearOfBirth > age.yearOfDeath - age.yearOfBirth){
+            age = person;
+        }
+        return age
+    }, people[0]);
 };
 
 // Do not edit below this line

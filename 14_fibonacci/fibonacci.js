883073ce2b1a3e080a9922cf0b7664012f82c76c
Bryan <bryanzin7@gmail.com>
2025-05-22 17:45:25 -0700

    completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    num = +number;
+    if (num < 0){
+        return "OOPS"
+    }else if(num === 0){
+        return 0
+    }else if(num === 1){
+        return 1
+    }else{
+        let a = 0;
+        let b = 1;
+        let fib = 0;
+        for(let i = 2;i<num+1;i++){
+            fib = a + b;
+            a = b;
+            b = fib;
+        };
+        return fib
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+/*
+Fibonacci secquence: 1, 1, 2, 3, 5, 8
+n = 2 => 0 + 1 = 1
+n = 3 => 1 + 1 = 2
+n = 4 => 1 + 2 = 3
+n = 5 => 2 + 3 = 5
+n = 6 => 3 + 5 = 8
+*/
